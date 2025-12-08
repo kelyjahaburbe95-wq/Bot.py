74adae342c62c353e06ff625d81910756b070caa
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-09 00:40:47 +0100

    Add files via upload

--- telegram_bot_2.py
+++ telegram_bot_2.py
@@ -4,14 +4,22 @@ from telegram.ext import Application, CommandHandler
 import asyncio
 import os
 
+# ---- FLASK ----
 app = Flask(__name__)
 
+# ---- TOKEN ----
 TOKEN = os.getenv("BOT_TOKEN")
 CANAL_PRINCIPAL = "https://t.me/+3RSkDPs9bS02NDZk"
 
+# ---- APPLICATION TG ----
 application = Application.builder().token(TOKEN).build()
 
-# --- COMMAND /start ---
+# ---- GLOBAL EVENT LOOP ----
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+
+# ---- START COMMAND ----
 async def start(update: Update, context):
     keyboard = [[InlineKeyboardButton("Canal principal 🔵", url=CANAL_PRINCIPAL)]]
     await update.message.reply_text(
@@ -19,10 +27,11 @@ async def start(update: Update, context):
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
+
 application.add_handler(CommandHandler("start", start))
 
 
-# --- WEBHOOK ROUTE ---
+# ---- WEBHOOK ROUTE ----
 @app.post("/webhook")
 def webhook():
     data = request.get_json()
@@ -31,9 +40,9 @@ def webhook():
         update = Update.de_json(data, application.bot)
 
         if not application._initialized:
-            asyncio.run(application.initialize())
+            loop.run_until_complete(application.initialize())
 
-        asyncio.run(application.process_update(update))
+        loop.create_task(application.process_update(update))
 
     return "OK", 200
 
@@ -43,7 +52,7 @@ def home():
     return "Bot Telegram en ligne ✔️"
 
 
-# --- RUN FLASK SERVER (OBLIGATOIRE SUR RENDER) ---
+# ---- RUN FLASK SERVER ----
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=PORT)
