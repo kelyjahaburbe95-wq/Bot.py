7a8d45638bb6d898f8301a7be3600b0fdaff32e6
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-10 00:21:57 +0100

    Add files via upload

--- telegram_bot_2.py
+++ telegram_bot_2.py
@@ -7,7 +7,7 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 
 CANAL_PRINCIPAL = "https://t.me/+3RSkDPs9bS02NDZk"
-NOUVEAU_CANAL = "https://t.me/tonNouveauCanal"
+Que Du Cul = "https://t.me/+3lW8pf60_hBhNDU0"
 
 app = Flask(__name__)
 application = Application.builder().token(TOKEN).build()
@@ -15,11 +15,14 @@ application = Application.builder().token(TOKEN).build()
 async def start(update: Update, context):
     keyboard = [
         [InlineKeyboardButton("Canal principal 🔵", url=CANAL_PRINCIPAL)],
-        [InlineKeyboardButton("Que Du Cul 🔞", url=QDCqueducul)]
-    
+        [InlineKeyboardButton("Que Du Cul 🔞 ", url=NOUVEAU_CANAL)]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     await update.message.reply_text(
         "Bienvenue sur le bot !\n\nChoisis un canal ci-dessous ⬇️",
-        reply_markup=InlineKeyboardMarkup(keyboard)
+        reply_markup=reply_markup
     )
 
 application.add_handler(CommandHandler("start", start))
@@ -27,7 +30,8 @@ application.add_handler(CommandHandler("start", start))
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json(force=True)
-    asyncio.run(application.process_update(Update.de_json(data, application.bot)))
+    update = Update.de_json(data, application.bot)
+    asyncio.run(application.process_update(update))
     return "OK", 200
 
 if __name__ == "__main__":
